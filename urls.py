e9ed4f149e28a75e7eacc997f9bfd950de07e95a
unknown <Carl Lee@.(none)>
2011-06-21 12:17:33 +0800

    Match the url '^$' into postlist

--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^$', 'posts.views.index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^posts/', include('posts.urls'))
 )
